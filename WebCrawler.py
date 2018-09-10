3c1e9448dd9ab954ae400ce060fe5ff071354256
Prescott Rowe <prescottrowe@gmail.com>
2018-09-10 14:00:39 -0700

    Working: Would be stronger with RegExp
    This is just a rough Web Crawling script for my use. I keep it in a barebones format to be able to easily add it into other programs.

--- WebCrawler.py
+++ WebCrawler.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup           #tools to sort through HTML to grab spec
 def spider(max_pages):
     page = 1
     while page <= max_pages:
-        url = 'https://www.nordstromrack.com/clearance/Men?page=' + str(page)
+        url = 'https://www.somesite.com/directory?page=' + str(page)
         html_code = requests.get(url)
         plain_text = html_code.text
         soup = BeautifulSoup(plain_text, 'html.parser')
         for link in soup.find_all('div', {'class': 'product-grid-item__brand'}):
-            #href= "https://www.nordstromrack.com" + link.get('href')
+            #href= "https://www.somesite.com" + link.get('href')
             title = link.string
             # print(href)
             print(title)
